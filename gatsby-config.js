9fc898bfc76d199f590097e65bef4f1bdb46c437
Matthew Cross <mattyc246@gmail.com>
2023-09-26 11:38:09 +0800

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,23 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Blog Demo",
+    title: 'Gatsby Blog Demo'
   },
   plugins: [
-    "gatsby-plugin-postcss",
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-postcss',
     {
-      resolve: "gatsby-source-datocms",
+      resolve: 'gatsby-source-datocms',
       options: {
         apiToken: process.env.DATO_API_TOKEN,
-        environment: process.env.DATO_ENVIRONMENT,
-      },
+        environment: process.env.DATO_ENVIRONMENT
+      }
     },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-  ],
+    `gatsby-transformer-sharp`
+  ]
 };
