408af70279d3ddbb4536a4a334e1ef6dbbc7d930
Matthew Cross <mattyc246@gmail.com>
2023-09-26 11:16:20 +0800

    use node

---
+++ gatsby-node.js
@@ -0,0 +1,26 @@
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allDatoCmsPost(sort: { order: DESC, fields: date }) {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+    }
+  `);
+
+  result.data?.allDatoCmsPost?.edges?.forEach(({ node: page }, index) => {
+    createPage({
+      path: `/posts/${page?.slug}`,
+      component: require.resolve('./src/pages/posts/post-template.js'),
+      context: {
+        id: page?.id
+      },
+      defer: index > 0 ? true : false
+    });
+  });
+};
